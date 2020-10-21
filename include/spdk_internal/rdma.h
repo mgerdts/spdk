fb3a285100d81338fcb863756aee685b19ddaf9a
Alexey Marchuk <alexeymar@mellanox.com>
2020-10-21 18:32:01 +0300

    rdma: Add functions to support memory translation
    This is the first commit in a series which aim is to
    unify MRs registration and memory translation in
    NVMEoF target and NVME initiator RDMA transports.
    
    This commit also adds a function to register
    some custom memory translation callback. This
    callback is different from approach implemented with
    nvme_rdma hooks since it allows to perfrom memory
    a custom translation in runtime.
    
    Next patches will add usage of new functions in
    RDMA transports.
    
    Change-Id: I93153d497fc4554ced14edbe545961b78bda91e3
    Signed-off-by: Alexey Marchuk <alexeymar@mellanox.com>

--- include/spdk_internal/rdma.h
+++ include/spdk_internal/rdma.h
@@ -38,6 +38,9 @@
 #include <rdma/rdma_cma.h>
 #include <rdma/rdma_verbs.h>
 
+/* Contains hooks definition */
+#include "spdk/nvme.h"
+
 struct spdk_rdma_qp_init_attr {
 	void		       *qp_context;
 	struct ibv_cq	       *send_cq;
@@ -56,6 +59,24 @@ struct spdk_rdma_qp {
 	struct ibv_qp *qp;
 	struct rdma_cm_id *cm_id;
 	struct spdk_rdma_send_wr_list send_wrs;
+};
+
+struct spdk_rdma_mem_map;
+
+union spdk_rdma_mr {
+	struct ibv_mr	*mr;
+	uint64_t	key;
+};
+
+enum SPDK_RDMA_TRANSLATION_TYPE {
+	SPDK_RDMA_TRANSLATION_MR = 0,
+	SPDK_RDMA_TRANSLATION_KEY
+};
+
+struct spdk_rdma_memory_translation {
+	union spdk_rdma_mr mr_or_key;
+	void *addr;
+	uint8_t translation_type;
 };
 
 /**
@@ -113,5 +134,64 @@ bool spdk_rdma_qp_queue_send_wrs(struct spdk_rdma_qp *spdk_rdma_qp, struct ibv_s
  * \return 0 on succes, errno on failure
  */
 int spdk_rdma_qp_flush_send_wrs(struct spdk_rdma_qp *spdk_rdma_qp, struct ibv_send_wr **bad_wr);
+
+
+/**
+ * Create a memory map which is used to register Memory Regions and perform address -> memory
+ * key translations
+ *
+ * \param pd Protection Domain which will be used to create Memory Regions
+ * \param hooks Structure with hooks which is used to create Protection Domain or ger RKey
+ * \return Pointer to memory map or NULL on failure
+ */
+struct spdk_rdma_mem_map *spdk_rdma_create_mem_map(struct ibv_pd *pd,
+		struct spdk_nvme_rdma_hooks *hooks);
+
+/**
+ * Free previously allocated memory map
+ *
+ * \param map Pointer to memory map to free
+ */
+void spdk_rdma_free_mem_map(struct spdk_rdma_mem_map **map);
+
+/**
+ * Prototype of the function to be used for custom memory translation.
+ *
+ * \param qp Pointer to RDMA qpair
+ * \param address Memory address for translation
+ * \param length Length of the memory address
+ * \param[in,out] translation Pointer to translation result to be filled by this function
+ * \retval -EINVAL if translation is not found
+ * \retval 0 translation succeed
+ */
+typedef int (*spdk_rdma_addr_translation_cb)(struct spdk_rdma_qp *qp, void *address, size_t length,
+		struct spdk_rdma_memory_translation *translation);
+
+/**
+ * Register a custom translation method which will be used for every memory translation in runtime.
+ *
+ * This approach differs from callbacks defined in spdk_nvme_rdma_hooks structure since they are only
+ * used to create Memory Regions, typically at initialization time.
+ *
+ * \param translation_cb Pointer to the translation function
+ */
+void spdk_rdma_register_translation_method(spdk_rdma_addr_translation_cb translation_cb);
+
+/**
+ * Get a translation for the given address and length.
+ *
+ * Note: the user of this function should use address returned in \b translation structure
+ *
+ * \param qp Pointer to RDMA qpair
+ * \param map Pointer to translation map
+ * \param address Memory address for translation
+ * \param length Length of the memory address
+ * \param[in,out] translation Pointer to translation result to be filled by this function
+ * \retval -EINVAL if translation is not found
+ * \retval -ERANGE if requested length exceeds Memory Region boundary
+ * \retval 0 translation succeed
+ */
+int spdk_rdma_get_translation(struct spdk_rdma_qp *qp, struct spdk_rdma_mem_map *map, void *address,
+			      size_t length, struct spdk_rdma_memory_translation *translation);
 
 #endif /* SPDK_RDMA_H */
