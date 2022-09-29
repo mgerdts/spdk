b7e33dd06b8c6f54b80f9d55dcd498a9d63f92eb
Mike Gerdts <mgerdts@nvidia.com>
2022-09-29 11:12:51 -0500

    lvol: add support for external snapshots (add copyright)
    Copyright updates are temporarily in a separate commit to allow fewer
    conflicts backporting to v22.05.nvda
    
    Change-Id: Iefbb863c0d1fbf0b45ea9f473a84f9758986eb5d
    Signed-off-by: Mike Gerdts <mgerdts@nvidia.com>

--- include/spdk_internal/lvolstore.h
+++ include/spdk_internal/lvolstore.h
@@ -1,6 +1,7 @@
 /*   SPDX-License-Identifier: BSD-3-Clause
  *   Copyright (c) Intel Corporation.
  *   All rights reserved.
+ *   Copyright (c) 2020-2022, NVIDIA CORPORATION. All rights reserved.
  */
 
 #ifndef SPDK_INTERNAL_LVOLSTORE_H
