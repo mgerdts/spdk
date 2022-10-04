7441f6c63a621ec416da441ead3b84b2028893cb
Andrii Holovchenko <andriih@nvidia.com>
2022-10-04 18:47:19 +0300

    contrib: update setup_hugepages
    Signed-off-by: Andrii Holovchenko <andriih@nvidia.com>

--- contrib/setup_hugepages.sh
+++ contrib/setup_hugepages.sh
@@ -2,4 +2,43 @@
 # Dependency on package:
 #   libhugetlbfs-utils @ CentOS
 #   hugepages @ Ubuntu
-exec /usr/bin/hugeadm --pool-pages-min DEFAULT:${MIN_HUGEMEM:-2G}
+
+min_hugemem=${MIN_HUGEMEM:-2G}
+Hugetlb=$(grep Hugetlb /proc/meminfo | awk '{ print $2 }')
+
+case $(echo ${min_hugemem: -1}) in
+    M)
+        unit=m
+        ;;
+    G)
+        unit=g
+        ;;
+    K)
+        unit=k
+        ;;
+    *)
+        echo "[ERROR]: Unsupported unit format for hugepages!"
+        exit 1
+        ;;
+esac
+
+if [ $Hugetlb -gt 0 ]; then
+    if [ $unit = "k" ]; then
+        required_size=${min_hugemem%?}
+    elif [ $unit = "m" ]; then
+	required_size=$((${min_hugemem%?} * 1024))
+    elif [ $unit = "g" ]; then
+	required_size=$((${min_hugemem%?} * 1024 * 1024))
+    fi
+
+    Hugepagesize=$(grep Hugepagesize /proc/meminfo | awk '{ print $2 }')
+    HugePages_Free=$(grep HugePages_Free /proc/meminfo | awk '{ print $2 }')
+
+    huge_free_size=$((Hugepagesize * HugePages_Free))
+
+    if [ $huge_free_size -ge $required_size ]; then
+        exit 0
+    fi
+fi
+
+exec /usr/bin/hugeadm --pool-pages-min DEFAULT:+${min_hugemem}
