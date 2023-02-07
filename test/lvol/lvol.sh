42058327d8a7039da311a0834b0303e3e5897f8e
Mike Gerdts <mgerdts@nvidia.com>
2023-02-06 18:48:05 -0600

    test/lvol: test esnap clones with real bdevs
    This adds test/lvol/esnap for functional tests lvol esnap clone bdevs
    without RPCs or reactors.
    
    v23.01.nvda merge notes
    
      - Conflict in test/lvol/lvol.sh
      - In sync with efd9e188a28006e7e4f01e79d05962747945b3be
    
    Change-Id: If62b1bde2b19343af51ba4c11599623556484b0d
    Signed-off-by: Mike Gerdts <mgerdts@nvidia.com>

--- test/lvol/lvol.sh
+++ test/lvol/lvol.sh
@@ -2,6 +2,7 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #  Copyright (C) 2019 Intel Corporation
 #  All rights reserved.
+#  Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
@@ -17,6 +18,7 @@ run_test "lvol_tasting" test/lvol/tasting.sh
 run_test "lvol_snapshot_clone" test/lvol/snapshot_clone.sh
 run_test "lvol_rename" test/lvol/rename.sh
 run_test "lvol_provisioning" test/lvol/thin_provisioning.sh
+run_test "lvol_esnap" test/lvol/esnap/esnap
 timing_exit basic
 
 timing_exit lvol
